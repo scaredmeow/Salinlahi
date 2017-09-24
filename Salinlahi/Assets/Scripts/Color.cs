aa7706e2a5ccf190b44f654ae3623a8d5eab0c44
scaredmeow <nlikemagic@users.noreply.github.com>
2017-09-25 00:06:03 +0800

    Initialize project

---
+++ Salinlahi/Assets/Scripts/Color.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour {
+
+	public GameObject box;
+	public Material materialRed; 
+	public Material materialBlue;
+	public Material materialGreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeColorToBlue () {
+		box.GetComponent<MeshRenderer> ().material = materialBlue;
+	}
+	public void ChangeColorToRed () {
+		box.GetComponent<MeshRenderer> ().material = materialRed;
+	}
+	public void ChangeColorToGreen () {
+		box.GetComponent<MeshRenderer> ().material = materialGreen;
+	}
+
+}
---
+++ Salinlahi/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveUp () {
+		box.GetComponent<Transform> ().position += Vector3.up;
+	}
+	public void MoveDown() {
+		box.GetComponent<Transform> ().position += Vector3.down;
+	}
+	public void MoveLeft() {
+		box.GetComponent<Transform> ().position += Vector3.left;
+	}
+	public void MoveRight() {
+		box.GetComponent<Transform> ().position += Vector3.right;
+	}
+}
